a169de310e2b03ec51a42111044c22ee5569f2d0
dell <pagaredivyanka@gmail.com>
2026-06-08 17:43:16 +0530

    class creats a users table have fields like id , email , passward , role , created_at

---
+++ src/main/java/com/divyanka/payment_gateway/entity/User.java
@@ -0,0 +1,35 @@
+package com.divyanka.payment_gateway.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = LocalDateTime.now();
+    }
+}
